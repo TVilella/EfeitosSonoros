dda6a63293bc9f316da0890ac163f5f40ad43fd2
mantovanidv <mantovanidv@gmail.com>
2020-04-26 21:43:53 -0300

    Update EfeitosSonoros.h
    conserto void sirene

--- src/EfeitosSonoros.h
+++ src/EfeitosSonoros.h
@@ -15,7 +15,7 @@ EfeitosSonoros(byte pino);
 void papararapa();
 void vitoria();
 void marchafunebre();
-void sirene();
+void sirene(int periodo = 10, int primeira = 100);
 
 private:
 void tocaefeito(int numerodenotas);
